5f3546bd949b20426857ce99f638e5fa1faed138
Nnabueze Opara <villavelle101@gmail.com>
2018-09-17 10:16:06 +0100

    project working

--- Support/Controllers/DefaultController.cs
+++ Support/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using Support.Models;
+﻿
+using Support.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,15 +32,45 @@ namespace Support.Controllers
             try
             {
                 var tinData = db.tins.Where(o => o.tin_no == tin || o.temporary_tin == tin).FirstOrDefault();
-                
+                if (tinData != null)
+                {
+                    Session["isLogin"] = "yes";
+                    Session["type"] = "IGR";
+                    SetInfo(tinData);
+                    return RedirectToAction("Index","Home");
+                }
+
+                ViewBag.error = "Invalid Tin No.";
+                return View("Index");
             }
             catch (Exception ex)
             {
                 ViewBag.error = ex.Message.ToString()+" | Please contact the Admin";
                 return View("Index");
             }
+        }
 
-            return View();
+        public bool SetInfo(Models.tin tinData)
+        {
+            Session["tinNo"] = tinData.tin_no;
+            Session["name"] = tinData.name;
+            Session["email"] = tinData.email;
+            Session["address"] = tinData.IGR_Code;
+            Session["temporary_tin"] = tinData.temporary_tin;
+            Session["phone"] = tinData.phone;
+            return true;
+        }
+
+        [Route("logout")]
+        public ActionResult Logout()
+        {
+            if (Session["isLogin"] != null)
+            {
+                Session.RemoveAll();
+                return RedirectToAction("Index","Default");
+            }
+
+            return RedirectToAction("Index", "Default");
         }
     }
 }
---
+++ Support/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Support.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["isLogin"] != null)
+            {
+                Console.WriteLine(Session["tinNo"] + " "+ Session["temporary_tin"]);
+                return View();
+            }
+
+            return RedirectToAction("Index","Default");
+        }
+    }
+}
