c5b655903ef5ba954e1fc24d734156bea2083751
Nnabueze Opara <villavelle101@gmail.com>
2018-10-05 09:16:47 +0100

    added payment list for school

--- Support/Controllers/SchoolController.cs
+++ Support/Controllers/SchoolController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Support.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,29 @@ namespace Support.Controllers
 {
     public class SchoolController : Controller
     {
+        support db = new support();
         // GET: School
         public ActionResult Index()
         {
             if (Session["isLogin"] != null)
             {
                 return View();
+            }
+
+            return RedirectToAction("SchoolLogin", "Default");
+        }
+
+        [Route("school/payments")]
+        public ActionResult payment()
+        {
+            if (Session["isLogin"] != null)
+            {
+                string AdmissionNo = Session["AdmissionNo"].ToString();
+                string IGR_Code = Session["igr"].ToString();
+
+                var info = db.student_information.Where(o => o.IGR_Code == IGR_Code && o.AdmissionNo == AdmissionNo).ToList();
+
+                return View(info);
             }
 
             return RedirectToAction("SchoolLogin", "Default");
